094e9f395c9b1c5eb00088bfe10647c26a4ac6ba
kathishivaramreddy <kathishivaram@gmail.com>
2021-01-31 13:37:58 +0530

    Retreive command of FeedStore delivers empty on empty cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -5,6 +5,21 @@
 import XCTest
 import FeedStoreChallenge
 
+class CodableFeedStore: FeedStore {
+	
+	func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+		
+	}
+	
+	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+		
+	}
+	
+	func retrieve(completion: @escaping RetrievalCompletion) {
+		completion(.empty)
+	}
+}
+
 class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	
 	//  ***********************
@@ -20,9 +35,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	//  ***********************
 	
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-		//		let sut = makeSUT()
-		//
-		//		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+		let sut = makeSUT()
+		
+		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
 	}
 	
 	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -94,7 +109,8 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	// - MARK: Helpers
 	
 	private func makeSUT() -> FeedStore {
-		fatalError("Must be implemented")
+		let sut = CodableFeedStore()
+		return sut
 	}
 	
 }
